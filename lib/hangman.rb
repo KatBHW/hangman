811e7492222b5ec3a6c6911cbb1d9d6b3a4c2fb6
Kathryn Williams <ksw4321@gmail.com>
2017-06-22 12:44:48 +0100

    incorrect guesses, invalid entries and messages improved

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,3 @@
-puts "Hangman initialized"
-
 word_list = File.readlines "wordlist.txt"
 
 # Create dictionary of words between 5 and 12 letters in length
@@ -13,28 +11,58 @@ all_words.each do |word|
   end
 end
 
-# Generate answer
+# Generate answer, answer board and array for storing incorrect guesses
 answer = dictionary.sample.chars
-puts answer.join # temporarily display answer to ease testing
- 
-# Generate board
 answer_board = Array.new(answer.length, " _")
-puts answer_board.join
+incorrect_letters = []
  
+# Welcome message
+puts "Welcome to Hangman." 
+puts
+puts "You have 12 attempts to guess the secret word which is between five and 12 letters long."
+puts
+ # temporarily display answer to ease testing
+puts "For those of you in the audience, the secret word is: #{answer.join}"
+
 # Play game 
-  selections_left = 3 # temporarily set at three to speed up testing
+  selections_left = 12 
   counter = 0 
   until (answer_board == answer) || (selections_left == 0)
-    puts "Please enter a letter:"
+    puts 
+    print "Your guess: "
     guess_letter = gets.chomp.to_s.downcase
-    answer.each_with_index do |letter, index|
-      if letter == guess_letter
-        answer_board[index] = guess_letter
-      end
-    end 
+      if (("a".."z").include?(guess_letter)) && (!answer.include?(guess_letter))
+        incorrect_letters << guess_letter  
+      elsif !("a".."z").include?(guess_letter)
+        puts "Invalid entry. Please try again."
+        selections_left += 1    
+      else  
+        answer.each_with_index do |letter, index|
+          if letter == guess_letter
+            answer_board[index] = guess_letter
+          end
+        end 
+      end 
     selections_left -= 1  
-    puts answer_board.join(" ")
+    puts 
+    print "Correct guesses: "
+    puts answer_board.join(" ").upcase
+    puts 
+    puts "Incorrect guesses: #{incorrect_letters.join(" ").upcase}"
+    puts 
+    puts "You have #{selections_left} guesses left."
+    puts "__________________________________"
+    if answer_board == answer
+      puts
+      puts "Well done. You correctly guessed the answer: #{answer.join.upcase}"
+    end  
   end
+puts
+puts "Bad luck. The answer was: #{answer.join.upcase}" if answer_board != answer
+puts
+
+
+  
 
 
  
