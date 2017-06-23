f4d76f2bd5a949f6b7978467994dac0c4e259009
Kathryn Williams <ksw4321@gmail.com>
2017-06-23 17:36:11 +0100

    option to save game added, check required on loading saved game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Game
 
   attr_reader :word_list, :dictionary, :answer, :answer_board, :incorrect_letters
@@ -61,23 +63,68 @@ class Game
       print "Correct guesses: "
       puts answer_board.join(" ").upcase
       puts "Incorrect guesses: #{incorrect_letters.join(" ").upcase}"
+      puts "Well done. You correctly guessed: #{answer.join.upcase}" if answer_board == answer
+        if (selections_left > 0) && (answer_board != answer)
+          print "Save game? Y/N: "
+          save_option = gets.chomp
+          if !save_option =~ /[YyNn]/
+            puts "Invalid entry. Please try again: "
+            save_option = gets.chomp
+          elsif save_option =~ /[Yy]/
+            save_game
+            exit  
+          end 
+        end    
+      puts "__________________________________"
       selections_left == 1? plural = "guess" : plural = "guesses"
       puts "You have #{selections_left} #{plural} left."
-      puts "__________________________________"
-      puts "Well done. You correctly guessed: #{answer.join.upcase}" if answer_board == answer
     end
     puts "Bad luck. The answer was: #{answer.join.upcase}" if answer_board != answer
     puts 
   end  
 
+# Save game 
+  def save_game
+	saved_data = [@answer, @answer_board, @incorrect_guesses, @selection_left]
+	File.open('saved_game.yml', 'w') { |n| n.write(YAML.dump(saved_data)) }
+	exit
+  end
+
+# Load saved game  
+  def load_saved_game
+    input_data = YAML.load(File.read('saved_game.yml'))
+    @answer = saved_data[0]
+    @answer_board = saved_data[1]
+    @incorrect_guesses = saved_data[2]
+    @selections_left = saved_data[3]
+  end
+
 # Play game 
-  def play_game
+  def play_new_game
     create_dictionary
     answers_and_guesses
     welcome
     game_turns
   end
+  
+  def play_saved_game
+    load_saved_game
+    game_turns
+  end
+
+ def select_game_type
+    print "Type 'L' to load a saved game or 'N' to start a new game: "
+    game_type = gets.chomp
+      if !game_type =~ /[LNln]/
+        print "Invalid entry. Please try again: "
+        game_type = gets.chomp
+      elsif game_type =~ /[Ll]/
+        play_saved_game
+      elsif game_type =~ /[Nn]/
+        play_new_game
+      end  
+  end
 end
 
 game = Game.new
-game.play_game
+game.select_game_type
